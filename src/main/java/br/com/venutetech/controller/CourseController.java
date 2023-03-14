028b0dc04861efc3c495fd17cb2bde85ec38999e
Neilson Venute <neilsonvenute@gmail.com>
2023-03-13 21:00:53 -0300

    método save habilitado para salvar no banco

--- src/main/java/br/com/venutetech/controller/CourseController.java
+++ src/main/java/br/com/venutetech/controller/CourseController.java
@@ -2,8 +2,14 @@ package br.com.venutetech.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.venutetech.model.Course;
@@ -14,11 +20,17 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/api/courses")
 @AllArgsConstructor
 public class CourseController {
-	
-	private final CourseRepository courseRepository;
+
+    private final CourseRepository courseRepository;
 
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course) ;
     }
 }
