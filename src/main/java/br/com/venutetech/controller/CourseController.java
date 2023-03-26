fb082ce27bf3f77486b8d43f18da1a6be603efda
Neilson Venute <neilsonvenute@gmail.com>
2023-03-26 19:48:14 -0300

    Criado o método de atualização do curso

--- src/main/java/br/com/venutetech/controller/CourseController.java
+++ src/main/java/br/com/venutetech/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,22 +22,32 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CourseController {
 
-    private final CourseRepository courseRepository;
+	private final CourseRepository courseRepository;
 
-    @GetMapping
-    public List<Course> list() {
-        return courseRepository.findAll();
-    }
+	@GetMapping
+	public List<Course> list() {
+		return courseRepository.findAll();
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable Long id) {
-        return courseRepository.findById(id).map(record -> ResponseEntity.ok().body(record))
-                .orElse(ResponseEntity.notFound().build());
-    }	
+	@GetMapping("/{id}")
+	public ResponseEntity<Course> findById(@PathVariable Long id) {
+		return courseRepository.findById(id).map(recordFound -> ResponseEntity.ok().body(recordFound))
+				.orElse(ResponseEntity.notFound().build());
+	}
 
-    @PostMapping
-    @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody Course course) {
-        return courseRepository.save(course);
-    }
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Course create(@RequestBody Course course) {
+		return courseRepository.save(course);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+		return courseRepository.findById(id).map(recordFound -> {
+			recordFound.setName(course.getName());
+			recordFound.setCategory(course.getCategory());
+			Course updated = courseRepository.save(recordFound);
+			return ResponseEntity.ok().body(updated);
+		}).orElse(ResponseEntity.notFound().build());
+	}
 }
